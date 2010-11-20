ec9ea0c61db59ba20d0d546c5cfda6632285e56e
yc <iyanchuan@gmail.com>
2010-11-20 20:13:14 +0800

    py-tools update

---
+++ python/py-tools/config.py
@@ -0,0 +1,2 @@
+EMAIL = 'test@gmail.com'
+PASSWORD = 'ba1f2511fc30423bdbb183fe33f3dd0f' # md5 hash of your password
--- python/py-tools/main.py
+++ python/py-tools/main.py
@@ -14,26 +14,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from urllib import urlencode
+import logging
+import re
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.api.urlfetch import fetch
+from google.appengine.api import memcache
+
+from config import *
 
 '''
 auto checkin for xiami.com
 '''
 
-TASKS = ['xiami_checkin']
-# log in to your xiami.com account, paste `javascript:alert($.cookie('auth'))` in the address
-# bar and hit enter, you'll get the following AUTH code
-AUTH ='60bRnVHLWWfAdKThkK/IoBGddQ10CHZAiEK11xUJSl4jRroPwE5F7m+Sr+PayYihLYk'
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
         self.response.out.write('Hello world!')
 
 class TaskHandler(webapp.RequestHandler):
+    '''
+    '''
+
+    headers = {
+        'Accept': 'application/json, text/javascript, */*',
+        'Accept-Language': 'zh-cn',
+        'Accept-Encoding': 'deflate',
+        'Accept-Charset': 'UTF-8,*;q=0.5',
+        'Content-Type': 'application/xml',
+        'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)',
+    }
+
     def get(self, task_id):
         '''
         task handler. Mostly, this is called by the cron task
@@ -41,40 +53,43 @@ class TaskHandler(webapp.RequestHandler):
         try:
             ret = {
                 'xiami_checkin': self._xiami_checkin,
+                'xiami_login': self._xiami_login,
             }[task_id]()
-        except:
-            self.response.set_status(404)
-            self.response.out.write('404 Not Found')
+        except Exception, e:
+            logging.error('Unable to complete task (%s)' % str(e))
             return
+        logging.info('Task done(ret: %s)' % str(ret))
 
-        if not ret:
-            self.response.out.write('Unable to complete task')
-        else:
-            self.response.out.write('Task done')
+    def _xiami_login(self):
+        '''
+        login xiami.com and grab the authed cookies
+        '''
+        self.headers.update({
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Cache-Control': 'no-cache',
+        })
+        auth = 'email=%s&password=%s&type=1&submit=1&autologin=1' % (EMAIL, PASSWORD)
+        obj = fetch('http://www.xiami.com/member/login', method='POST', headers=self.headers, follow_redirects=False, payload=auth)
+        ret = re.search(r'^(auth=[^;]+)', obj.headers['set-cookie']).group()
+        memcache.set('cookie', ret)
+        return ret
 
     def _xiami_checkin(self):
         '''
         checkin task for xiami.com
         '''
-        headers = {
-            'Accept': 'application/json, text/javascript, */*',
-            'Accept-Language': 'zh-cn,zh;q=0.5',
-            'Accept-Encoding': 'deflate',
-            'Accept-Charset': 'UTF-8,*;q=0.5',
-            'Content-Type': 'application/xml',
-            'Referer': 'http://www.xiami.com/',
-            'X-Requested-With': 'XMLHttpRequest',
-            'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12',
-            'Cookie': urlencode({'auth': AUTH}),
-        }
-        try:
-            obj = fetch('http://www.xiami.com/task/signin', method='POST', headers=headers)
-            ret = obj.content.strip()
-        except Exception, e:
-            ret = ''
-        finally:
-            return bool(ret)
+        cookie = memcache.get('cookie')
+        if cookie is None:
+            self._xiami_login()
+            cookie = memcache.get('cookie')
 
+        self.headers.update({
+            'X-Requested-With': 'XMLHttpRequest',
+            'Referer': 'http://www.xiami.com/',
+            'Cookie': cookie,
+        })
+        obj = fetch('http://www.xiami.com/task/signin', method='POST', headers=self.headers)
+        return obj.content.strip()
 
 def main():
     application = webapp.WSGIApplication([
