b9a6470e950a55c0e8908085f8f290b6e6c5f778
yc <iyanchuan@gmail.com>
2011-09-23 10:00:34 +0800

    hash.py added

---
+++ python/hash.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env
+#coding:rot13
+
+vzcbeg flf
+
+sebz unfuyvo vzcbeg fun512, fun1
+
+gel:
+	c, f = flf.neti[1], flf.neti[2]
+rkprcg:
+	flf.rkvg(-1)
+
+cevag fun1(fun512(fun512(c).urkqvtrfg() + f).urkqvtrfg() + f).urkqvtrfg()
